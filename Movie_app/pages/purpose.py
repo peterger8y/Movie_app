638a4b93ee9c58c8d5239f3aa4344fbec3cecc30
peterger8y <73508520+peterger8y@users.noreply.github.com>
2021-03-08 03:36:10 -0500

    Update purpose.py

--- Movie_app/pages/purpose.py
+++ Movie_app/pages/purpose.py
@@ -17,13 +17,11 @@ column1 = dbc.Col(
             """      
 Potential Uses:
 
-This app is intended for use by larger, experienced corporations to see what changes to a film productions features result in potential revenue outputs.
-It includes a prediction function to check projected revenues with given feature inputs. Using these in conjunction with a budget can provide the means for a straightfoward 
-return on investment calculation.
+This app is intended for use by larger/experienced corporations. It includes a prediction function to check projected revenues with given feature inputs.
+Using these in conjunction with a budget can provide the means for a straightfoward return on investment calculation.
 
-The Movie_app, (or any other program/app like it) should not be used for final production decisions. Three features included in the original data set that were highly predictive, but excluded
-due to potential feature leakage, had to do with popularity and critical review. Therefore, any major production film, in addition to research and modeling projections, should include 
-a distinctive and powerful creative and powerful creative vision. Suffice to say, films can be over-analyzed and over produced. 
+The Movie_app, (or any other program/app like it) should be used lightly for final production decisions. Three features included in the original data set(highly predictive) were excluded
+due to potential feature leakage. These features had to do with popularity and critical review.
 
 Please enjoy the use of this app!
 
